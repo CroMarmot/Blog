5d46d7dcc3f317fd3d8a905b2c1faef6a4735239
cromarmot <yexiaorain@gmail.com>
2021-02-23 16:10:10 +0800

    scripts: CD: github action

--- update.sh
+++ update.sh
@@ -9,4 +9,4 @@
 ######################################################################
 
 git add . && git commit -m "update & commit by script" && git push origin master
-rm -rf public && hexo g && cd public && git init && git checkout -b gh-pages && git add . && git commit -m "update" && git remote add origin git@github.com:CroMarmot/Blog.git && git push origin gh-pages -f && cd ../
+# rm -rf public && hexo g && cd public && git init && git checkout -b gh-pages && git add . && git commit -m "update" && git remote add origin git@github.com:CroMarmot/Blog.git && git push origin gh-pages -f && cd ../
